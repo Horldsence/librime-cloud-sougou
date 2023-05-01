b473ea1bc0b51386bb57855060c808b018a30f75
Horldsence <92992976+Horldsence@users.noreply.github.com>
2023-05-01 19:20:50 +0800

    Update rime.lua

--- scripts/rime.lua
+++ scripts/rime.lua
@@ -1,8 +1,13 @@
 
---- 百度云拼音，Control+t 为云输入触发键
+--- 百度云及搜狗云拼音，Ctrl+t 和 Ctrl+g 分别为百度云和搜狗云输入触发键
 --- 使用方法：
---- 将 "lua_translator@baidu_translator" 和 "lua_processor@baidu_processor"
---- 分别加到输入方案的 engine/translators 和 engine/processors 中
-local baidu = require("trigger")("Control+t", require("baidu"))
+--- 需要哪一个即更改哪一个
+--- 将 "lua_translator@baidu_translator" 和 "lua_processor@baidu_processor"分别加到输入方案的 engine/translators 和 engine/processors 中
+local baidu = require("trigger_baidu")("Control+t", require("baidu"))
 baidu_translator = baidu.translator
 baidu_processor = baidu.processor
+--- 将 "lua_translator@sougou_translator" 和 "lua_processor@sougou_processor"分别加到输入方案的 engine/translators 和 engine/processors 中
+
+local sougou = require("trigger_sougou")("Control+g", require("sougou"))
+sougou_translator = sougou.translator
+sougou_processor = sougou.processor
